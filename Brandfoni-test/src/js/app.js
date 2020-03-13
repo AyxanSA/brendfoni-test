f32c65b1795aee8ede852d97e820aadd42de1325
AyxanSA <50141881+AyxanSA@users.noreply.github.com>
2020-03-13 19:42:14 +0400

    finished header

--- Brandfoni-test/src/js/app.js
+++ Brandfoni-test/src/js/app.js
@@ -5,12 +5,22 @@ $(function () {
   $('#alert').on('click', e => alert('clicked'))
 });
 
-
 var input = document.querySelector('input');
-
 var result = document.querySelector('.search-result');
-
-input.addEventListener('input', function()
-{
+input.addEventListener('input', function () {
   result.style.display = "block";
 });
+
+var icon1 = document.getElementsByClassName('user-profil')[0];
+var activClass = document.getElementsByClassName('active')[0];
+var layer = document.getElementsByClassName('layer')[0];
+
+icon1.addEventListener("click", function () {
+  console.log("clicked");
+  activClass.style.display = "block";
+  layer.style.display = "block";
+});
+layer.addEventListener("click", function(){
+  activClass.style.display = "none";
+  layer.style.display = "none";
+})
