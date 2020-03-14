d027039c4506da99ca91548fa70b4786d1c817bc
AyxanSA <50141881+AyxanSA@users.noreply.github.com>
2020-03-14 20:08:34 +0400

    main-content

--- Brandfoni-test/src/js/app.js
+++ Brandfoni-test/src/js/app.js
@@ -4,13 +4,6 @@ import 'bootstrap/dist/js/bootstrap';
 $(function () {
   $('#alert').on('click', e => alert('clicked'))
 });
-
-var input = document.querySelector('input');
-var result = document.querySelector('.search-result');
-input.addEventListener('input', function () {
-  result.style.display = "block";
-});
-
 var icon1 = document.getElementsByClassName('user-profil')[0];
 var activClass = document.getElementsByClassName('active')[0];
 var layer = document.getElementsByClassName('layer')[0];
@@ -20,7 +13,27 @@ icon1.addEventListener("click", function () {
   activClass.style.display = "block";
   layer.style.display = "block";
 });
-layer.addEventListener("click", function(){
+layer.addEventListener("click", function () {
   activClass.style.display = "none";
   layer.style.display = "none";
+  result.style.display = "none";
+  input.value = "";
 })
+var input = document.querySelector('input');
+var result = document.querySelector('.search-result');
+input.addEventListener('input', function () {
+  result.style.display = "block";
+  layer.style.display = "block";
+});
+var marka = document.getElementsByClassName("marka-search");
+var brends = document.getElementsByClassName("search-box")[0];
+
+
+for (var i = 0; i < marka.length; i++) {
+  marka[i].addEventListener("click", function () {
+    var parent = this.parentElement;
+    $(this).parent().siblings().removeClass('active');
+    $(this).parent().toggleClass("active");
+  });
+}
+
